a91a4bdb1fde4b47b505aa09600cf9210fe38bea
Aaron Thapa <notsmart98@gmail.com>
2023-12-15 13:44:48 +0545

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -227,12 +227,11 @@ int main() {
     return 0;
 }
 
-void timer_init()
-{
+void timer_init() {
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-    
+
     GPIO_PinRemapConfig(GPIO_FullRemap_TIM2, ENABLE);
 
     GPIO_InitTypeDef GPIO_InitStructure = {0};
@@ -241,13 +240,12 @@ void timer_init()
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
 
-    TIM2->PSC = 72-1;
+    TIM2->PSC = SystemCoreClock / 1000000 - 1;
     TIM2->ATRLR = 1000;
     TIM2->CNT = 0;
     TIM2->CHCTLR2 = 0b0000000001100000;
     TIM2->CCER = 0b0000000100000000;
     TIM2->CH3CVR = 500;
-
 }
 
 void exti_init() {
