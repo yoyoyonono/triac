9fc62e1efa69b22f9a4932d1c78c0f474b2f38bd
Aaron Thapa <notsmart98@gmail.com>
2023-12-17 22:14:28 +0545

    convert to separate tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <ch32v10x.h>
 #include <debug.h>
 #include <string.h>
+#include <FreeRTOS.h>
+#include <task.h>
 
-#include "ch32v10x_it.h"
 #include "display.hpp"
 #include "tkey.hpp"
 #include "util.hpp"
@@ -60,7 +61,14 @@
 #define TRIAC_PORT GPIOA
 #define TRIAC_PIN 12
 
-#define FIRE_LENGTH_us 600
+#define TASK_TRIAC_PRIORITY 5
+#define TASK_TRIAC_STACK_SIZE 256
+#define TASK_DISPLAY_PRIORITY 5
+#define TASK_DISPLAY_STACK_SIZE 256
+#define TASK_BUTTON_PRIORITY 5
+#define TASK_BUTTON_STACK_SIZE 256
+
+#define FIRE_LENGTH_us 100
 
 extern "C" void EXTI15_10_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 
@@ -71,8 +79,8 @@ uint64_t get_tick();
 
 const uint16_t wattage_delay_lookup[] = {8640, 8064, 7616, 7216, 6864, 6544, 6224, 5920, 5632, 5344, 5056, 4768, 4480, 4192, 3904, 3600, 3264, 2928, 2560, 2128, 1616, 864};
 
-volatile uint16_t firing_delay = 8704;
-uint16_t target_firing_delay = 8640;
+volatile uint16_t firing_delay_us = 8704;
+uint16_t target_firing_delay_us = 8640;
 uint16_t current_wattage = 200;
 uint8_t read_count = 0;
 uint64_t last_buzzer = 0;
@@ -83,6 +91,8 @@ static uint16_t time_ms = 0;
 bool switch_states[] = {false, false, false, false, false, false};
 bool previous_switch_states[] = {false, false, false, false, false, false};
 const bool all_false_switches[] = {false, false, false, false, false, false};
+
+volatile bool toFire = false;
 
 TouchButton touch[] = {
     TouchButton(ADC_Channel_0, 1900),
@@ -105,6 +115,89 @@ Display display = Display(SEG_A_PORT, SEG_A_PIN,
                           SEG_DIG3_PORT, SEG_DIG3_PIN,
                           SEG_DIG4_PORT, SEG_DIG4_PIN,
                           COMMON_CATHODE);
+
+TaskHandle_t task_triac_handle;
+TaskHandle_t task_display_handle;
+TaskHandle_t task_button_handle;
+
+void task_triac(void *pvParameters) {
+    while (true) {
+        if (firing_delay_us != target_firing_delay_us) {
+            if (firing_delay_us < target_firing_delay_us) {
+                firing_delay_us += 8;
+            } else {
+                firing_delay_us -= 8;
+            }
+        }
+        if (toFire) {
+            vTaskDelay(firing_delay_us/1000);
+            delayMicroseconds(firing_delay_us % 1000);
+            digitalWrite(TRIAC_PORT, TRIAC_PIN, HIGH);
+            delayMicroseconds(FIRE_LENGTH_us);
+            digitalWrite(TRIAC_PORT, TRIAC_PIN, LOW);
+        }
+    }
+}
+
+void task_display(void *pvParameters) {
+    while (true) {
+        display.refresh();
+        vTaskDelay(1);
+        display.allOff();
+    }
+}
+
+void task_button(void *pvParameters) {
+    while (true) {
+        for (uint8_t i = 0; i < 6; i++) {
+            switch_states[i] = touch[i].is_pressed();
+#ifdef LOG_BUTTONVALS
+            printf("%d ", touch[i].read());
+#endif
+        }
+#ifdef LOG_BUTTONVALS
+        printf("\r\n");
+#endif
+        read_count++;
+
+        if (memcmp(switch_states, previous_switch_states, 6) == 0) {
+            continue;
+        }
+        memcpy(previous_switch_states, switch_states, 6);
+#ifdef LOG_SWITCHES
+        printf("change\t");
+        for (bool i : switch_states) {
+            printf("%d", i);
+        }
+        printf("\r\n");
+#endif
+        if (memcmp(switch_states, all_false_switches, 6) == 0) {
+            continue;
+        }
+
+        if (switch_states[0]) {
+            current_wattage -= 200;
+            if (current_wattage < 200) {
+                current_wattage = 200;
+            }
+        }
+        if (switch_states[5]) {
+            current_wattage += 200;
+            if (current_wattage > 2000) {
+                current_wattage = 2000;
+            }
+        }
+        display.printNumber(current_wattage);
+        target_firing_delay_us = wattage_to_delay(current_wattage);
+#ifdef LOG_SWITCHES
+        printf("Wattage: %d\tDelay: %d\r\n", current_wattage, target_firing_delay);
+#endif
+        TIM2->CTLR1 |= 1;
+        vTaskDelay(250);
+        TIM2->CTLR1 &= (~1);
+    }
+}
+
 
 
 int main() {
@@ -185,65 +278,32 @@ int main() {
 
     printf("%d", (int)get_tick());
 
-    while (true) {
-        if (firing_delay != target_firing_delay) {
-            if (firing_delay < target_firing_delay) {
-                firing_delay += 8;
-            } else {
-                firing_delay -= 8;
-            }
-        }
-        for (int i = 0; i < 16; i++) {
-            display.refresh();
-        }
-        display.allOff();
-        if (get_tick() - last_buzzer > time_ms * 125) {
-            TIM2->CTLR1 &= (~1);
-        }
-        for (uint8_t i = 0; i < 6; i++) {
-            switch_states[i] = touch[i].is_pressed();
-#ifdef LOG_BUTTONVALS
-            printf("%d ", touch[i].read());
-#endif
-        }
-#ifdef LOG_BUTTONVALS
-        printf("\r\n");
-#endif
-        read_count++;
+    xTaskCreate((TaskFunction_t)task_triac,
+                (const char *)"triac",
+                (uint16_t)TASK_TRIAC_STACK_SIZE,
+                (void *)NULL,
+                (UBaseType_t)TASK_TRIAC_PRIORITY,
+                (TaskHandle_t *)&task_triac_handle);
+    
+    xTaskCreate((TaskFunction_t)task_display,
+                (const char *)"display",
+                (uint16_t)TASK_DISPLAY_STACK_SIZE,
+                (void *)NULL,
+                (UBaseType_t)TASK_DISPLAY_PRIORITY,
+                (TaskHandle_t *)&task_display_handle);
+    
+    xTaskCreate((TaskFunction_t)task_button,
+                (const char *)"button",
+                (uint16_t)TASK_BUTTON_STACK_SIZE,
+                (void *)NULL,
+                (UBaseType_t)TASK_BUTTON_PRIORITY,
+                (TaskHandle_t *)&task_button_handle);
 
-        if (memcmp(switch_states, previous_switch_states, 6) == 0) {
-            continue;
-        }
-        memcpy(previous_switch_states, switch_states, 6);
-#ifdef LOG_SWITCHES
-        printf("change\t");
-        for (bool i : switch_states) {
-            printf("%d", i);
-        }
-        printf("\r\n");
-#endif
-        if (memcmp(switch_states, all_false_switches, 6) == 0) {
-            continue;
-        }
-        TIM2->CTLR1 |= 1;
-        last_buzzer = get_tick();
-        if (switch_states[0]) {
-            current_wattage -= 200;
-            if (current_wattage < 200) {
-                current_wattage = 200;
-            }
-        }
-        if (switch_states[5]) {
-            current_wattage += 200;
-            if (current_wattage > 2000) {
-                current_wattage = 2000;
-            }
-        }
-        display.printNumber(current_wattage);
-        target_firing_delay = wattage_to_delay(current_wattage);
-#ifdef LOG_SWITCHES
-        printf("Wattage: %d\tDelay: %d\r\n", current_wattage, target_firing_delay);
-#endif
+    printf("Tasks created\r\n");
+
+    vTaskStartScheduler();
+
+    while (true) {
     }
     return 0;
 }
@@ -292,14 +352,10 @@ void exti_init() {
 extern "C" void EXTI15_10_IRQHandler(void) {
     if (EXTI_GetITStatus(EXTI_Line15) != RESET) {
         // turn off all digits
-        display.allOff();
 #ifdef LOG_INTERRUPT
         printf("EXTI15_10_IRQHandler\r\n");
 #endif
-        delayMicroseconds(firing_delay);
-        digitalWrite(TRIAC_PORT, TRIAC_PIN, HIGH);
-        delayMicroseconds(FIRE_LENGTH_us);
-        digitalWrite(TRIAC_PORT, TRIAC_PIN, LOW);
+        toFire = true;
         EXTI_ClearITPendingBit(EXTI_Line15); /* Clear Flag */
     }
 }
